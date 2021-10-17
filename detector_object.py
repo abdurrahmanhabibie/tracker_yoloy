0537808ff36ada9cf8a91fd8afa331d78317dcdf
abdurrahmanhabibie <49420324+abdurrahmanhabibie@users.noreply.github.com>
2021-10-17 21:01:22 +0700

    Update detector_object.py

--- detector_object.py
+++ detector_object.py
@@ -6,7 +6,6 @@ import cv2
 import numpy as np
 import time
 import darknet
-from tracker.darknet import IMAGE
 
 def convertBack(x, y, w, h): 
     #================================================================
@@ -41,18 +40,17 @@ def cvDrawBoxes(detections, img):
     if len(detections) > 0:  						# At least 1 detection in the image and check detection presence in a frame  
         centroid_dict = dict() 						# Function creates a dictionary and calls it centroid_dict
         objectId = 0								# We inialize a variable called ObjectId and set it to 0
-        for detection in detections:				# In this if statement, we filter all the detections for persons only
-            # Check for the only person name tag 
-            name_tag = str(detection[0].decode())   # Coco file has string of all the names           
-            x, y, w, h = detection[2][0],\
-                        detection[2][1],\
-                        detection[2][2],\
-                        detection[2][3]      	# Store the center points of the detections
+        for label, confidence, bbox in detections:				# In this if statement, we filter all the detections for persons only
+            x, y, w, h = (bbox[0],
+                    bbox[1],
+                    bbox[2],
+                    bbox[3])
+            name_tag = label      	# Store the center points of the detections
             xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))   # Convert from center coordinates to rectangular coordinates, We use floats to ensure the precision of the BBox            
             # Append center point of bbox for persons detected.
             centroid_dict[objectId] = (int(x), int(y), xmin, ymin, xmax, ymax) # Create dictionary of tuple with 'objectId' as the index center points and bbox
-            objectId += 1 #Increment the index for each detection     
-            print(centroid_dict)
+            objectId += 1 #Increment the index for each detection
+            print(centroid_dict)      
     #=================================================================#
     return img
 
@@ -67,7 +65,7 @@ def YOLO():
     """
     global metaMain, netMain, altNames
     configPath = "./cfg/custom-yolov4-tiny-detector.cfg"
-    weightPath = "./custom-yolov4-tiny-detector_best.weights"
+    weightPath = "./custom-yolov4-tiny-detector_last.weights"
     metaPath = "./data/obj.data"
     if not os.path.exists(configPath):
         raise ValueError("Invalid config path `" +
@@ -79,10 +77,7 @@ def YOLO():
         raise ValueError("Invalid data file path `" +
                          os.path.abspath(metaPath)+"`")
     if netMain is None:
-        netMain = darknet.load_net_custom(configPath.encode(
-            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
-    if metaMain is None:
-        metaMain = darknet.load_meta(metaPath.encode("ascii"))
+        network, class_names, class_colors = darknet.load_network(configPath,  metaPath, weightPath, batch_size=1)
     if altNames is None:
         try:
             with open(metaPath) as metaFH:
@@ -99,19 +94,21 @@ def YOLO():
                         with open(result) as namesFH:
                             namesList = namesFH.read().strip().split("\n")
                             altNames = [x.strip() for x in namesList]
+                            
                 except TypeError:
                     pass
+            print(namesList)
         except Exception:
             pass
     #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("./Input/test_park.mp4") #perlu diubah sesuai nama file yang diinginkan
+    cap = cv2.VideoCapture("./nextar2.mp4") #perlu diubah sesuai nama file yang diinginkan
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     new_height, new_width = frame_height // 2, frame_width // 2
     print("Video Reolution: ",(frame_width, frame_height))
 
     out = cv2.VideoWriter(
-            "./Demo/Hasil_park1.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 10.0,
+            "./Demo/Hasil.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 10.0,
             (new_width, new_height))
     
     # print("Starting the YOLO loop...")
@@ -133,12 +130,12 @@ def YOLO():
 
         darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
 
-        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        detections = darknet.detect_image(network, class_names, darknet_image, thresh=0.25)
         image = cvDrawBoxes(detections, frame_resized)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         #print(1/(time.time()-prev_time))
-        cv2.imshow('Demo', image)
-        cv2.waitKey(3)
+        # cv2.imshow('Demo', image)
+        # cv2.waitKey(3)
         out.write(image)
 
     cap.release()
